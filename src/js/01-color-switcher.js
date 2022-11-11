87416faf4c172a2d657887084d224b7a6080a26b
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-11 18:15:38 +0200

    added task # 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startChangeColorBtnEl = document.querySelector('[data-start]');
+const stopChangeColorBtnEl = document.querySelector('[data-stop]');
 
+startChangeColorBtnEl.addEventListener('click', startChangeColorFn);
+stopChangeColorBtnEl.addEventListener('click', stopChangeColorFn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+function startChangeColorFn() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startChangeColorBtnEl.disabled = true;
+  stopChangeColorBtnEl.disabled = false;
+}
+
+function stopChangeColorFn() {
+  clearInterval(intervalId);
+  startChangeColorBtnEl.disabled = false;
+  stopChangeColorBtnEl.disabled = true;
+}
